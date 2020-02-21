ee4868f3e38dfb8f4a647f354c7af8e72cc7c1e3
M3dicCookie <pred102@hotmail.com>
2020-02-20 20:13:13 -0800

    uploaded files

---
+++ NoShinyForYou/NoShinyForYou.cs
@@ -0,0 +1,36 @@
+﻿using QModManager.API.ModLoading;
+using Harmony;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+namespace NoShinyForYou
+{
+    [QModCore]
+    public class Entry
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            try
+            {
+                var harmony = HarmonyInstance.Create("Cookie.NoShinyForYou");
+                    harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SeaMonkeyStealShiny))]
+    [HarmonyPatch(nameof(SeaMonkeyStealShiny.Evaluate))]
+    class SeaMonkeyStealShiny_Evaluate_Patch
+    {
+        public static void Postfix(ref float __result)
+        {
+            __result = 0f;
+        }
+    }
+}
